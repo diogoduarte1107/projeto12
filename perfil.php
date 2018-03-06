18ebdf8b01dba3b16235712810185f410b45049f
diogoduarte1107 <diogoduarte1107@gmail.com>
2018-03-05 21:42:16 -0300

    adicionando perfil

---
+++ perfil.php
@@ -0,0 +1,37 @@
+<?php 
+include'head.php';
+ ?>
+
+
+
+<section class="conteudo">
+
+			
+				<figure class="produtos"/>
+				<img src="produtos/perfil.jpeg" class="tamanho"/>
+				<figcaption>
+									</figcaption>
+			</figure>
+			
+		<table>
+			
+					
+				<tr> 
+					<th>Turma</th>
+					<th>Ano</th>
+					<th>Idade</th>
+					<th>Pai</th>
+					<th>Mãe</th>
+				</tr>
+				<tr class="colimp">
+					<td>700</td>
+					<td>5°</td>
+					<td>9anos</td>
+					<td>José</td>
+					<td>Maria</td>
+				
+			   </fieldset>
+		    </form>
+		</section>
+
+
